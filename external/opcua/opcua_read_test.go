f98f9ea253a7d34ba066aa1eb938589498ba82ac
dimon <delmore.li@aliyun.com>
2024-12-27 11:45:12 +0800

    feat: 抽取公共OPC UA 客户端；添加OPC UA读写节点

---
+++ external/opcua/opcua_read_test.go
@@ -0,0 +1,58 @@
+package opcua
+
+import (
+	"encoding/json"
+	"testing"
+
+	opcuaClient "github.com/rulego/rulego-components-iot/pkg/opcua_client"
+	"github.com/rulego/rulego/api/types"
+	"github.com/rulego/rulego/test"
+	"github.com/rulego/rulego/test/assert"
+)
+
+func TestReadNode(t *testing.T) {
+	Registry := &types.SafeComponentSlice{}
+	Registry.Add(&ReadNode{})
+	var nodeType = "x/opcUaRead"
+
+	// t.Run("NewNode", func(t *testing.T) {
+	// 	test.NodeNew(t, nodeType, &ReadNode{}, types.Configuration{
+	// 		"server": "opc.tcp://127.0.0.1:53530/OPCUA/SimulationServer",
+	// 		"policy": "none",
+	// 		"mode":   "none",
+	// 		"auth":   "anonymous",
+	// 	}, Registry)
+	// })
+	nodeIds := make([]string, 0)
+	nodeIds = append(nodeIds, "ns=3;i=1001")
+	nodeIds = append(nodeIds, "ns=3;i=1009")
+	d, _ := json.Marshal(nodeIds)
+
+	meta := types.BuildMetadata(make(map[string]string))
+	meta.PutValue("nodeIds", string(d))
+
+	msgList := []test.Msg{
+		{
+			MetaData: meta,
+			DataType: types.JSON,
+			MsgType:  opcuaClient.OPC_UA_DATA_MSG_TYPE,
+		},
+	}
+
+	t.Run("NodeOnMsg", func(t *testing.T) {
+		node, _ := test.CreateAndInitNode(nodeType, types.Configuration{
+			"server": "opc.tcp://127.0.0.1:53530/OPCUA/SimulationServer",
+			"policy": "none",
+			"mode":   "none",
+			"auth":   "anonymous",
+		}, Registry)
+
+		test.NodeOnMsg(t, node, msgList, func(msg types.RuleMsg, relationType string, err error) {
+			assert.Equal(t, types.Success, relationType)
+			assert.Nil(t, err)
+			assert.NotNil(t, msg.Data)
+			t.Logf("data : %s ", msg.Data)
+		})
+	})
+
+}
